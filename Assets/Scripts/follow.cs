e6998d46c81f525e11cdd4a5d2decafb26a0267d
anxiaoyu_liu <15273271228@163.com>
2018-08-14 15:35:02 +0800

    test

---
+++ Assets/Scripts/follow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow : MonoBehaviour {
+    public Transform playerweizhi;
+    private Vector3 juli;
+	// Use this for initialization
+	void Start () {
+        juli= transform.position - playerweizhi.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = playerweizhi.position + juli;
+	}
+}
